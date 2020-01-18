f1d59d462905aa573e16435d9e01004551ae48a0
baijingfeng <2596239285@qq.com>
2020-01-18 23:08:50 +0800

    update

--- NodeJsStudy/demo9/index.js
+++ NodeJsStudy/demo9/index.js
@@ -1,5 +1,5 @@
 const os = require("os")
 
-console.log(os.cpus())
+console.log(os.cpus()) // 打印出本机的CPU信息
 
-console.log(os.totalmem()/1024/1024/1024)
+console.log(os.totalmem() / 1024 / 1024 / 1024) // 打印出本机的内存信息
