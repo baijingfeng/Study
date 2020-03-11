8f99d25be351dad350e9d86d074d36513507e18b
baijingfeng <bai.jingfeng@qq.com>
2020-03-11 18:36:44 +0800

    update: js

--- NodeJsStudy/test.js
+++ NodeJsStudy/test.js
@@ -22,8 +22,55 @@ function test(arr) {
 var enter = '22 5555 22 666 00 88 888 7777 4444 666 44'
 
 function jiugongge(enter) {
-	const charDictArr = ['0 ', '1,.?!', '2ABC', '3DEF', '4GHI', '5JKL', '6MNO', '7PQRS', '8TUV', '9WXYZ']
-	for(let i = 0; i < enter.length; i++) {
-		
+	const key = ['0 ', '1,.?!', '2ABC', '3DEF', '4GHI', '5JKL', '6MNO', '7PQRS', '8TUV', '9WXYZ']
+
+	let rst = ''
+	let begin = 0
+	let end = 0
+	for (let i = 0; i < enter.length; i++) {
+		if (enter[i] === ' ' || i === enter.length - 1) {
+			let count = end - begin
+
+			if (i === enter.length - 1) {
+				count++
+			}
+
+			let index = (count - 1) % key[enter[begin] - '0'].length
+
+			rst += key[enter[begin] - '0'][index]
+			begin = i + 1
+		}
+		end++
 	}
+	console.log(rst)
+	console.log('\n')
+}
+
+/** 航班预定座位数 */
+
+var bookings = [
+		[1, 2, 10],
+		[2, 3, 20],
+		[2, 5, 25]
+	]
+var n = 5
+
+function flightBookings(bookings, n) {
+	// let range = (start, end) => new Array(end - start).fill(start).map((el, i) => start + i);
+	let rst = new Array(n).fill(0)
+
+	for (let [i, j, num] of bookings) {
+		rst[i - 1] += num
+
+		if (j < n) {
+			rst[j] -= num
+		}
+	}
+
+	for (let k = 1; k < n; k++) {
+		rst[k] += rst[k - 1]
+	}
+
+	console.log(rst)
+	return rst
 }
