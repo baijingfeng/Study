9510306dfa5cdc9cca375ea96a8044a0ec6d1225
baijingfeng <bai.jingfeng@qq.com>
2020-07-21 10:50:03 +0800

    update

---
+++ LagouStudy/module1/01-higher-order-function.js
@@ -0,0 +1,15 @@
+// 高阶函数-函数作为参数
+
+// function forEach(callback, array) {
+//   for (let )
+// }
+
+const _ = require("lodash");
+const fp = require("lodash/fp");
+
+const array = ["jack", "tom", "machiel", "lucy"];
+
+console.log(_.first(array));
+console.log(_.last(array));
+console.log(_.first(array));
+console.log(_.last(array));
